15733ba1dffa5ed74f943542f881b8884eede607
xmoulin <xavier.moulin@sopragroup.com>
2014-01-03 01:27:46 +0100

    ajout de tests sur le NFC

--- NFC/NFC.ino
+++ NFC/NFC.ino
@@ -34,6 +34,8 @@
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);        // Create MFRC522 instance.
+byte previousNfcUID[7];
+byte nfcUID[7];
 
 void setup() {
         Serial.begin(9600);        // Initialize serial communications with the PC
@@ -46,15 +48,71 @@ void loop() {
         // Look for new cards
         if ( ! mfrc522.PICC_IsNewCardPresent()) {
                //Serial.println("PICC_IsNewCardPresent");
-                return;
+               return;
         }
 
         // Select one of the cards
         if ( ! mfrc522.PICC_ReadCardSerial()) {
                //Serial.println("PICC_ReadCardSerial");
+                resetPreviousValue();
                 return;
         }
 
         // Dump debug info about the card. PICC_HaltA() is automatically called.
-        mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+        //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+        	
+        int nfcUIDLength = mfrc522.uid.size;
+        //Serial.print("mfrc522.uid.size=");
+        //Serial.println(mfrc522.uid.size);
+        
+        for (byte count = 0; count < mfrc522.uid.size; count++) {
+	  nfcUID[count]=mfrc522.uid.uidByte[count];
+	}
+
+        if (!isSameNFCCard()) {       
+          Serial.print("Card UID:");
+      	  for (byte i = 0; i < nfcUIDLength; i++) {
+      		Serial.print(nfcUID[i] < 0x10 ? " 0" : " ");
+      		Serial.print(nfcUID[i], HEX);
+      	  } 
+  	  Serial.println();
+          copyActualToPrevious();
+        }
+        
+	// Dump UID
+	/*Serial.print("Card UID:");
+	for (byte i = 0; i < mfrc522.uid.size; i++) {
+		Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+		Serial.print(mfrc522.uid.uidByte[i], HEX);
+	} 
+	Serial.println();*/
+}
+
+boolean isSameNFCCard() {
+  for (byte i = 0; i < 7; i++) {
+    if (previousNfcUID[i] != nfcUID[i]) { 
+      Serial.print("isSameNFCCard KO, i=");
+      Serial.print(i);
+      Serial.print("previousNfcUID[i]=");
+      Serial.print(previousNfcUID[i]);
+      Serial.print("nfcUID[i]=");
+      Serial.println(nfcUID[i]);
+      return false;
+    }
+  } 
+  return true;
+}
+
+void copyActualToPrevious() {
+        Serial.println("nouvelle carte, on memorise");
+        for (byte i = 0; i < 7; i++) {
+	  previousNfcUID[i]=nfcUID[i];
+	}
+}
+
+void resetPreviousValue() {
+        Serial.println("reset NFC");
+	for (byte i = 0; i < 7; i++) {
+		previousNfcUID[i] = 0;
+	} 
 }
