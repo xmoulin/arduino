c2b9896ee549373259c1ac502b416f091e38e9d6
xmoulin <xavier.moulin@sopragroup.com>
2019-09-07 14:34:21 +0200

    ruban claever cloud

---
+++ ledRubanClever/ledRubanClever.ino
@@ -0,0 +1,145 @@
+#include <Adafruit_NeoPixel.h>    // https://github.com/adafruit/Adafruit_NeoPixel
+#include <ESP8266WiFi.h>
+#include <WebSocketClient.h>      // https://github.com/morrissinger/ESP8266-Websocket
+
+#define PIXEL_COUNT       30
+#define WIFI_SSID         "Wifi_Moulin"
+#define WIFI_PASSWORD     "BaptisteEtPauline"
+//#define WIFI_SSID         "iPhone de xmoulin"
+//#define WIFI_PASSWORD     "sopra123"
+#define WEBSOCKET_HOST    "app-430b518a-b99f-45b2-9de3-2fc9ad2cf63f.cleverapps.io"
+#define WEBSOCKET_PORT    80
+#define WEBSOCKET_PATH    "/ws"
+
+struct RGB {
+  byte r;
+  byte g;
+  byte b;
+};
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, 02, NEO_GRB + NEO_KHZ800);
+WebSocketClient wsClient;
+WiFiClient client;
+
+RGB color;
+RGB lastColor;
+int scrolling;
+
+bool connected;
+
+void setup() {
+  Serial.begin(9600); // Used for debugging messages
+
+  // Initialize the colors
+  lastColor = {0, 0, 0};
+  color = {0, 0, 0};
+  scrolling = 0;
+  strip.begin();
+  strip.show();
+
+  setup_wifi();
+  setup_websocket();
+}
+
+void loop() {
+  // If connection fails / breaks, the first LED blinks red
+  if (!client.connected() || !connected) {
+    renderLEDColor(0, {255, 0, 0});
+    delay(500);
+    renderLEDColor(0, {0, 0, 0});
+    delay(500);
+  } else if (scrolling > 0) { // If scrolling is greater than 0, render LEDs
+    renderLED(strip.numPixels() - scrolling);
+    delay(20);
+    scrolling--;
+  } else {
+    // When receiving data, process it and start scrolling animation
+    // Expected format is: RRRGGGBBB (as in 255000000, 035127078, ...)
+    // There is no error handling
+    String data;
+    wsClient.getData(data);
+    if (data.length() > 0) {
+      Serial.print("Received: ");
+      Serial.println(data);
+      int red   = data.substring(0, 3).toInt() / 255.0 * 100;
+      int green = data.substring(3, 6).toInt() / 255.0 * 100;
+      int blue  = data.substring(6, 9).toInt() / 255.0 * 100;
+      Serial.print("Red: ");
+      Serial.println(red);
+      Serial.print("Green: ");
+      Serial.println(green);
+      Serial.print("Blue: ");
+      Serial.println(blue);
+
+      // Update colors
+      lastColor = color;
+      color = {red, green, blue};
+
+      // Start scrolling animation
+      scrolling = strip.numPixels();
+    }
+  }
+}
+
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("WiFi is connected on ");
+  Serial.println(WIFI_SSID);
+  Serial.print("=> IP address : ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup_websocket() {
+  wsClient.host = WEBSOCKET_HOST;
+  wsClient.path = WEBSOCKET_PATH;
+  Serial.println("Initiating connection...");
+  if(client.connect(WEBSOCKET_HOST, WEBSOCKET_PORT)) {
+    Serial.println("Connected.");
+    Serial.println("Initializing websocket...");
+    if(wsClient.handshake(client)) {
+      Serial.println("Handshake successful.");
+      connected = true;
+    } else {
+      Serial.println("Handshake failed.");
+      connected = false;
+    }
+  } else {
+    Serial.println("Connection failed.");
+    connected = false;
+  }
+}
+
+double diffAbs(byte last, byte current) {
+  return (double) (last > current ? last - current : current - last);
+}
+
+byte computeValueAt(byte last, byte current, int i) {
+  double ratio = (double) i / (double) strip.numPixels();
+  return (current + (last > current ? 1 : -1) * (byte) (diffAbs(last, current) * ratio));
+}
+
+void renderLED(int i) {
+  byte r = computeValueAt(lastColor.r, color.r, i);
+  byte g = computeValueAt(lastColor.g, color.g, i);
+  byte b = computeValueAt(lastColor.b, color.b, i);
+  strip.setPixelColor(i, r, g, b);
+  strip.show();
+}
+
+void renderLEDColor(int i, RGB color) {
+  strip.setPixelColor(i, color.r, color.g, color.b);
+  strip.show();
+}
